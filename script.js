3e66d0a07f778a91a9181cf468c4820ebe96597c
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-05 15:57:53 +0100

    Changing color of the target, for example location when user clicks li

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // DOM variables
 let p_openingHours = document.querySelector('#openingHours');
+let p_address = document.querySelector('#address');
+let p_contact = document.querySelector('#phone');
+let li4 = document.querySelector('#li4');
+let li5 = document.querySelector('#li5');
+
+li4.addEventListener('click', event => {
+    p_address.style.color = '#E68F0E';
+    setTimeout(function(){ p_address.style.color = 'white'; }, 750);
+});
+
+li5.addEventListener('click', event => {
+    p_contact.style.color = '#E68F0E';
+    setTimeout(function(){ p_contact.style.color = 'white'; }, 750);
+})
 
 let openingHours = {
     weekdays: '07:00 - 22:00',
